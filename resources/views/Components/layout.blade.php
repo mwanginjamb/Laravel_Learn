f331b6c6bf5386dd8b4bf5c0d50e4327a7066763
mwanginjamb <fnjambi@outlook.com>
2024-08-28 18:12:20 +0300

    Feat: Add a job controller for better structuring

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>My Learning App {{ $title }}</title>
+    <title>My Learning App {{ $title ?? 'Home' }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
